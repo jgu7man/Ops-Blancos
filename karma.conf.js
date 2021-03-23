24876bb7bedd55eb79752a78995e27110c40e78f
jgu7man <jgu7man@gmail.com>
2021-03-23 09:50:52 -0600

    base dependecies installed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/whites-balance'),
+      dir: require('path').join(__dirname, './coverage/ops-blancos'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'whites-balance'`, () => {
+  it(`should have as title 'ops-blancos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('whites-balance');
+    expect(app.title).toEqual('ops-blancos');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('whites-balance app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ops-blancos app is running!');
   });
 });
